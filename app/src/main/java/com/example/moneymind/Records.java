648df8021f097ff87ee29b0980b737e20ec5554b
Chaitanya-ghutukade <chaitanyaghutukade90@gmail.com>
2024-01-20 23:29:24 +0530

    updated raw data for recyclerview

--- app/src/main/java/com/example/moneymind/Records.java
+++ app/src/main/java/com/example/moneymind/Records.java
@@ -33,14 +33,14 @@ ArrayList<Record_Model> arr_records=new ArrayList<>();
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        arr_records.add(new Record_Model(R.drawable.baseline_food_bank_24,"food and drink","cash","gpay","100rs","today"));
-        arr_records.add(new Record_Model(R.drawable.baseline_food_bank_24,"food and drink","cash","gpay","100rs","today"));
-        arr_records.add(new Record_Model(R.drawable.baseline_food_bank_24,"food and drink","cash","gpay","100rs","today"));
-        arr_records.add(new Record_Model(R.drawable.baseline_food_bank_24,"food and drink","cash","gpay","100rs","today"));
-        arr_records.add(new Record_Model(R.drawable.baseline_food_bank_24,"food and drink","cash","gpay","100rs","today"));
-        arr_records.add(new Record_Model(R.drawable.baseline_food_bank_24,"food and drink","cash","gpay","100rs","today"));
-        arr_records.add(new Record_Model(R.drawable.baseline_food_bank_24,"food and drink","cash","gpay","100rs","today"));
-        arr_records.add(new Record_Model(R.drawable.baseline_food_bank_24,"food and drink","cash","gpay","100rs","today"));
+        arr_records.add(new Record_Model(R.drawable.tools,"food and drink","cash","gpay","100rs","today"));
+        arr_records.add(new Record_Model(R.drawable.tools,"food and drink","cash","gpay","110rs","today"));
+        arr_records.add(new Record_Model(R.drawable.tools,"food and drink","cash","gpay","200rs","today"));
+        arr_records.add(new Record_Model(R.drawable.tools,"food and drink","cash","gpay","150rs","today"));
+        arr_records.add(new Record_Model(R.drawable.tools,"food and drink","cash","gpay","189rs","today"));
+        arr_records.add(new Record_Model(R.drawable.tools,"food and drink","cash","gpay","135rs","today"));
+        arr_records.add(new Record_Model(R.drawable.tools,"food and drink","cash","gpay","102rs","today"));
+        arr_records.add(new Record_Model(R.drawable.tools,"food and drink","cash","gpay","12rs","today"));
 
 
         RecyclerRecordsAdapter adapter=new RecyclerRecordsAdapter(this,arr_records);
