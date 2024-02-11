c601e0c47c4bf291013c51722257117701478e95
Chaitanya-ghutukade <chaitanyaghutukade90@gmail.com>
2024-02-11 23:26:16 +0530

    added close and check icon in add record activity

--- app/src/main/java/com/example/moneymind/Add_Records.java
+++ app/src/main/java/com/example/moneymind/Add_Records.java
@@ -6,8 +6,11 @@ import androidx.core.content.ContextCompat;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Add_Records extends AppCompatActivity {
 
@@ -17,12 +20,24 @@ public class Add_Records extends AppCompatActivity {
         setContentView(R.layout.activity_add_records);
         Toolbar toolbar=findViewById(R.id.Add_Records_toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Add Records");
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(ContextCompat.getColor(this, R.color.add_record_toolbar_color));
         }
+
+        ImageView close=findViewById(R.id.close_icon);
+        ImageView check=findViewById(R.id.check_icon);
+
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
